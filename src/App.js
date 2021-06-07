8c10d3b89ba9dfbaa36f71d33a6a54376ff8da31
Lena Kujath <l.kujath@gmx.de>
2021-06-07 14:06:53 +0200

    cleans up and adds basic structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <h1>Title</h1>
+        <textarea/>
+        <h4>Amout of time</h4>
+        <button>Start</button>
+        <h1>0</h1>
+    </>
   );
 }
 
