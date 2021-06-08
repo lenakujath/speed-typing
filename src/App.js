a67af707b262254a731f0f104b1f23f5d441c21e
Lena Kujath <l.kujath@gmx.de>
2021-06-08 12:42:24 +0200

    makes starting the game dependent on a click event

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ function App() {
 
     const [text, setText] = useState("");
     const [timer, setTimer] = useState(5);
-
+    const [isRunning, setIsRunning] = useState(false);
+ 
     const wordCounter = (text) => {
       const wordArr = text.trim().split(' ');
       return wordArr.filter(word => word !== '').length;
     };
-
+    
     useEffect(() => { 
-      if (timer === 0) {
-        return;
+      
+      if (timer > 0 && isRunning){
+        setTimeout(() => {
+          setTimer(prevState => prevState -1)
+        }, 1000);
+      } else if (timer === 0) {
+        setIsRunning(false);
       }
-       setTimeout(() => {
-         setTimer(prevState => prevState -1)
-       }, 1000);
-       }, [timer]);
+       }, [isRunning, timer]);
 
   return (
     <>
@@ -28,7 +31,7 @@ function App() {
         onChange={(e) => setText(e.target.value)}
         />
         <h4>Time left: {timer}</h4>
-        <button onClick={() => wordCounter(text)}>Start</button>
+        <button onClick={() => setIsRunning(true)}>Start</button>
         <h1>Words: 0</h1>
     </>
   );
