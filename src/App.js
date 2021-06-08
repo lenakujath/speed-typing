8fd025d6066ba138f9941c925a61be8e4b781089
Lena Kujath <l.kujath@gmx.de>
2021-06-08 11:14:03 +0200

    sets a counter function and a timer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+    const [text, setText] = useState("");
+    const [timer, setTimer] = useState(5);
+
+    const wordCounter = (text) => {
+      const wordArr = text.trim().split(' ');
+      return wordArr.filter(word => word !== '').length;
+    };
+
+    useEffect(() => { 
+      if (timer === 0) {
+        return;
+      }
+       setTimeout(() => {
+         setTimer(prevState => prevState -1)
+       }, 1000);
+       }, [timer]);
+
   return (
     <>
         <h1>Title</h1>
-        <textarea/>
-        <h4>Amout of time</h4>
-        <button>Start</button>
-        <h1>0</h1>
+        <textarea
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        />
+        <h4>Time left: {timer}</h4>
+        <button onClick={() => wordCounter(text)}>Start</button>
+        <h1>Words: 0</h1>
     </>
   );
 }
